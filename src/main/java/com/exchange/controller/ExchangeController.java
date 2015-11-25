bfec54b52c531c918bce3e72e34b25874342ed80
Madziowiec <magda.sadlo@gmail.com>
2015-11-25 07:58:32 +0100

    clean up

--- src/main/java/com/exchange/controller/ExchangeController.java
+++ src/main/java/com/exchange/controller/ExchangeController.java
@@ -1,6 +1,9 @@
 package com.exchange.controller;
 
-import com.exchange.handler.SAXHandler;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.XML;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,10 +12,9 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.*;
 import java.io.IOException;
 import java.net.URL;
-
-/**
- * Created by Magda on 2015-11-23.
- */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class ExchangeController {
@@ -20,22 +22,33 @@ public class ExchangeController {
     @ResponseBody
     @RequestMapping("/api/today/CHF")
     public String getTodayExchangeRate() throws IOException, SAXException, ParserConfigurationException {
-        final SAXHandler handler = new SAXHandler();
-        handler.setCurrency("CHF");
-        handler.setPeriod("2015-11-23");
-        final SAXParserFactory factory = SAXParserFactory.newInstance();
+        List<JSONObject> rateList;
         try {
-            final SAXParser parser = factory.newSAXParser();
-            parser.parse(new URL("http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml").openConnection().getInputStream(), handler);
-        } catch (final ParserConfigurationException | IOException | SAXException e) {
+            String text = IOUtils.toString(new URL("http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml").openConnection().getInputStream());
+            JSONObject jsonObject = XML.toJSONObject(text);
+            JSONArray jsonArray = ((JSONArray)((JSONObject)((JSONObject)XML.toJSONObject(text).get("gesmes:Envelope")).get("Cube")).get("Cube"));
+            List<JSONObject> list= jsonArrayToArray(jsonArray);
+            Optional<JSONObject> optionalRatesAtDate = list.stream().filter(el -> el.get("time").equals("2015-11-24")).findAny();
+            JSONObject ratesAtDate = optionalRatesAtDate.get();
+            rateList = jsonArrayToArray((JSONArray)ratesAtDate.get("Cube"));
+            return rateList.stream().filter(el -> el.get("currency").equals("CHF")).findAny().get().get("rate").toString();
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        return handler.getResult();
+        return "";
     }
 
     @ResponseBody
     @RequestMapping("/api/2015-11-10/CHF")
     public String getExchangeRateOnDate() {
         return "0.8";
+    }
+
+    private List<JSONObject> jsonArrayToArray(JSONArray jsonArray) {
+        List<JSONObject> jsonObjects = new ArrayList<>();
+        for (Object jsonObject : jsonArray) {
+            jsonObjects.add(((JSONObject)jsonObject));
+        }
+        return jsonObjects;
     }
 }
